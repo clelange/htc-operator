b2512bc883cd61ae08a2c0f7e58953bba7e394a0
Tadas Bareikis <tbareiki@tadocentos7.cern.ch>
2020-04-24 01:50:13 +0200

    Updated CI file

--- ci/operator-sdk-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -sSL 'https://github.com/operator-framework/operator-sdk/releases/download/v0.17.0/operator-sdk-v0.17.0-x86_64-linux-gnu' > operator-sdk
-chmod +x operator-sdk
-./operator-sdk build randname
