22d6b459cbc84d2b0cb47f4b2d6ad5612edd6e08
Clemens Lange <clemens.lange@cern.ch>
2020-07-23 21:47:19 +0200

    Clean up scripts and configs

--- recreate.sh
+++ recreate.sh
@@ -1,11 +1,12 @@
-#operator-sdk generate k8s
+#!/bin/sh
+# operator-sdk generate k8s
 go build -o build/bin/receiver cloudevents/receiver.go
 go build -o build/bin/sender cloudevents/sender.go
-#operator-sdk build xkxgygmoqkguuddnkz/htc-operator --image-build-args "--build-arg CI_PROJECT_NAMESPACE=cms-cloud --build-arg CI_PROJECT_NAME=htc-operator" || exit
-CGO_ENABLED=1 GOOS=linux operator-sdk build xkxgygmoqkguuddnkz/htc-operator|| exit
-docker push xkxgygmoqkguuddnkz/htc-operator || exit
+# operator-sdk build gitlab-registry.cern.ch/cms-cloud/htc-operator --image-build-args "--build-arg CI_PROJECT_NAMESPACE=cms-cloud --build-arg CI_PROJECT_NAME=htc-operator" || exit
+CGO_ENABLED=1 GOOS=linux operator-sdk build gitlab-registry.cern.ch/cms-cloud/htc-operator|| exit
+docker push gitlab-registry.cern.ch/cms-cloud/htc-operator || exit
 
-kubectl create configmap s3cfg --from-file=$HOME/.s3cfg
+kubectl create configmap s3cfg --from-file="${HOME}/.s3cfg"
 
 kubectl delete -f deploy/operator.yaml
 kubectl delete -f deploy/crds/htc.cern.ch_htcjobs_crd.yaml
